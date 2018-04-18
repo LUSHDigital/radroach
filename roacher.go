af279c0237a477ee027b2087ab044477ede53d48
Dan Richards <dan.richards@lush.co.uk>
2018-04-18 15:04:37 +0100

    Table constraints now added at the end of the ouput file as ALTER TABLE statements

--- roacher.go
+++ roacher.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"regexp"
 )
 
 var (
+	empty = []byte("")
+
 	simpleReplacements = map[*regexp.Regexp][]byte{
-		regexp.MustCompile("`"):                                         []byte(""),
-		regexp.MustCompile(`^.* ENGINE=.*$/\)`):                         []byte(""),
+		regexp.MustCompile("`"):                                         empty,
+		regexp.MustCompile(`^.* ENGINE=.*$/\)`):                         empty,
 		regexp.MustCompile(" double "):                                  []byte(" FLOAT "),
 		regexp.MustCompile(` int\((.*)\) `):                             []byte(" INT "),
 		regexp.MustCompile(` double\((.*)\) `):                          []byte(" FLOAT "),
@@ -22,8 +25,8 @@ var (
 		regexp.MustCompile(` enum\((.*)\) `):                            []byte(" TEXT "),
 		regexp.MustCompile("  KEY "):                                    []byte("  INDEX "),
 		regexp.MustCompile("  FULLTEXT KEY "):                           []byte("  INDEX "),
-		regexp.MustCompile("ON UPDATE CURRENT_TIMESTAMP"):               []byte(""),
-		regexp.MustCompile(" ON DELETE CASCADE"):                        []byte(""),
+		regexp.MustCompile("ON UPDATE CURRENT_TIMESTAMP"):               empty,
+		regexp.MustCompile(" ON DELETE CASCADE"):                        empty,
 		regexp.MustCompile("DEFAULT b'0',$"):                            []byte("DEFAULT 0,"),
 		regexp.MustCompile("DEFAULT b'1',$"):                            []byte("DEFAULT 1,"),
 		regexp.MustCompile(`int DEFAULT '(.*)',$`):                      []byte("INT DEFAULT $1,"),
@@ -36,6 +39,7 @@ var (
 	blankLines     = regexp.MustCompile(`\s\s\n`)
 	constraints    = regexp.MustCompile("(?smU)CONSTRAINT.*REFERENCES.*$")
 	tables         = regexp.MustCompile("(?smU)^(CREATE|INSERT).*;$")
+	tableName      = regexp.MustCompile(`CREATE TABLE "(.*)"`)
 	trailingCommas = regexp.MustCompile(`,\n\);`)
 )
 
@@ -56,19 +60,36 @@ func (r *roacher) roach() (output []byte, err error) {
 	// Split the source data down by table.
 	tables := tables.FindAll(output, -1)
 
-	var tableConstraints [][]byte
+	tableConstraints := make(map[string][][]byte)
 	for i := range tables {
 		t := &tables[i]
 
-		// TODO: We need to allocate constraints against the table name here.
+		name := tableName.FindSubmatch(*t)
+		if name == nil {
+			continue
+		}
 
 		// Extract the constraints from each table definition.
-		tableConstraints = append(tableConstraints, constraints.FindAll(*t, -1)...)
-		*t = constraints.ReplaceAll(*t, []byte(""))
+		tableConstraints[string(name[1])] = constraints.FindAll(*t, -1)
+		*t = constraints.ReplaceAll(*t, empty)
 
 		// Tidy.
-		*t = blankLines.ReplaceAll(*t, []byte(""))
+		*t = blankLines.ReplaceAll(*t, empty)
 		*t = trailingCommas.ReplaceAll(*t, []byte("\n);"))
+	}
+
+	for table, constraints := range tableConstraints {
+		for _, constraint := range constraints {
+			constraint = bytes.Replace(constraint, []byte(","), empty, -1)
+			constraint = append(constraint, []byte(";")...)
+
+			tableConstraint := append(
+				[]byte(fmt.Sprintf("ALTER TABLE %s ADD ", table)),
+				constraint...,
+			)
+
+			tables = append(tables, tableConstraint)
+		}
 	}
 
 	output = bytes.Join(tables, []byte("\n"))
