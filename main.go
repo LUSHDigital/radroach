50e95193ae96c4636c5ed1d1b45e763cc6974574
Dan Richards <dan.richards@lush.co.uk>
2018-04-18 13:19:26 +0100

    CONSTRAINT lines are now extracted from the table definitions for re-writing at the end of the output file. Slight refactor of some var names. Output file is now working as an import into crdb (albeit without constraints in place)

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 
 type options struct {
 	verbose  bool
-	srcMYSQL string
-	destCRDB string
+	srcMySQL string
+	dstCrDB  string
 }
 
 var opts options
@@ -37,32 +37,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	opts.srcMYSQL = flag.Args()[0]
-	if len(opts.srcMYSQL) == 0 {
+	opts.srcMySQL = flag.Args()[0]
+	if len(opts.srcMySQL) == 0 {
 		usage()
 		os.Exit(1)
 	}
 
-	opts.destCRDB = flag.Args()[1]
-	if len(opts.destCRDB) == 0 {
+	opts.dstCrDB = flag.Args()[1]
+	if len(opts.dstCrDB) == 0 {
 		usage()
 		os.Exit(1)
 	}
 
 	logger := verboseLogger{}
 
-	srcInfo, err := os.Stat(opts.srcMYSQL)
+	srcInfo, err := os.Stat(opts.srcMySQL)
 	if err != nil {
-		logger.Log(fmt.Errorf("could not stat source file %q: %s", opts.srcMYSQL, err))
+		logger.Log(fmt.Errorf("could not stat source file %q: %s", opts.srcMySQL, err))
 
 		fmt.Println("Hmm, couldn't open the source mysql file for reading. Make sure the path and permissions are correct.")
 		os.Exit(1)
 	}
 
 	// TODO: Does reading and parsing line-by-line make more sense?
-	srcData, err := ioutil.ReadFile(opts.srcMYSQL)
+	srcData, err := ioutil.ReadFile(opts.srcMySQL)
 	if err != nil {
-		logger.Log(fmt.Errorf("could not read file %q: %s", opts.srcMYSQL, err))
+		logger.Log(fmt.Errorf("could not read file %q: %s", opts.srcMySQL, err))
 
 		fmt.Println("Hmm, couldn't read the source mysql file for reading. Make sure the path and permissions are correct.")
 		os.Exit(1)
@@ -72,15 +72,15 @@ func main() {
 
 	crdbData, err := roacher.roach()
 	if err != nil {
-		logger.Log(fmt.Errorf("could not roachify mysql data: %s", err))
+		logger.Log(fmt.Errorf("could not roach mysql data: %s", err))
 
 		fmt.Println("Damn, couldn't convert the mysql dump to crdb. Make sure the source file was prepared correctly.")
 		os.Exit(1)
 	}
 
-	err = ioutil.WriteFile(opts.destCRDB, crdbData, srcInfo.Mode())
+	err = ioutil.WriteFile(opts.dstCrDB, crdbData, srcInfo.Mode())
 	if err != nil {
-		logger.Log(fmt.Errorf("could not save the crdb data to file %q: %s", opts.destCRDB, err))
+		logger.Log(fmt.Errorf("could not save the crdb data to file %q: %s", opts.dstCrDB, err))
 
 		fmt.Println("Oh dear, couldn't write the crdb data. Make sure you have the correct permissions.")
 		os.Exit(1)
--- roacher.go
+++ roacher.go
@@ -1,33 +1,43 @@
 package main
 
-import "regexp"
+import (
+	"bytes"
+	"regexp"
+)
 
-var simpleReplacements = map[*regexp.Regexp][]byte{
-	regexp.MustCompile("`"):                                         []byte(""),
-	regexp.MustCompile(`^.* ENGINE=.*$/\)`):                         []byte(""),
-	regexp.MustCompile(" double "):                                  []byte(" FLOAT "),
-	regexp.MustCompile(` int\((.*)\) `):                             []byte(" INT "),
-	regexp.MustCompile(` double\((.*)\) `):                          []byte(" FLOAT "),
-	regexp.MustCompile(` bigint\((.*)\) `):                          []byte(" INT "),
-	regexp.MustCompile(` binary\((.*)\) `):                          []byte(" BYTES "),
-	regexp.MustCompile(` tinyint\((.*)\) `):                         []byte(" INT "),
-	regexp.MustCompile(" datetime "):                                []byte(" TIMESTAMP "),
-	regexp.MustCompile(" mediumtext "):                              []byte(" TEXT "),
-	regexp.MustCompile(" unsigned "):                                []byte(" "),
-	regexp.MustCompile(" mediumtext,"):                              []byte(" TEXT,"),
-	regexp.MustCompile(` enum\((.*)\) `):                            []byte(" TEXT "),
-	regexp.MustCompile("  KEY "):                                    []byte("  INDEX "),
-	regexp.MustCompile("  FULLTEXT KEY "):                           []byte("  INDEX "),
-	regexp.MustCompile("ON UPDATE CURRENT_TIMESTAMP"):               []byte(""),
-	regexp.MustCompile("ON DELETE CASCADE"):                         []byte(""),
-	regexp.MustCompile("DEFAULT b'0',$"):                            []byte("DEFAULT 0,"),
-	regexp.MustCompile("DEFAULT b'1',$"):                            []byte("DEFAULT 1,"),
-	regexp.MustCompile(`int DEFAULT '(.*)',$`):                      []byte("INT DEFAULT $1,"),
-	regexp.MustCompile(`int NOT NULL DEFAULT '(.*)',$`):             []byte("INT NOT NULL DEFAULT $1,"),
-	regexp.MustCompile(`\(decimal(.*)\) NOT NULL DEFAULT '(.*)',$`): []byte("$1 NOT NULL DEFAULT $2,"),
-	regexp.MustCompile(`\(decimal(.*)\) DEFAULT '(.*)',$`):          []byte("$1 DEFAULT $2,"),
-	regexp.MustCompile("  UNIQUE KEY "):                             []byte("  UNIQUE INDEX "),
-}
+var (
+	simpleReplacements = map[*regexp.Regexp][]byte{
+		regexp.MustCompile("`"):                                         []byte(""),
+		regexp.MustCompile(`^.* ENGINE=.*$/\)`):                         []byte(""),
+		regexp.MustCompile(" double "):                                  []byte(" FLOAT "),
+		regexp.MustCompile(` int\((.*)\) `):                             []byte(" INT "),
+		regexp.MustCompile(` double\((.*)\) `):                          []byte(" FLOAT "),
+		regexp.MustCompile(` bigint\((.*)\) `):                          []byte(" INT "),
+		regexp.MustCompile(` binary\((.*)\) `):                          []byte(" BYTES "),
+		regexp.MustCompile(` tinyint\((.*)\) `):                         []byte(" INT "),
+		regexp.MustCompile(" datetime "):                                []byte(" TIMESTAMP "),
+		regexp.MustCompile(" mediumtext "):                              []byte(" TEXT "),
+		regexp.MustCompile(" unsigned "):                                []byte(" "),
+		regexp.MustCompile(" mediumtext,"):                              []byte(" TEXT,"),
+		regexp.MustCompile(` enum\((.*)\) `):                            []byte(" TEXT "),
+		regexp.MustCompile("  KEY "):                                    []byte("  INDEX "),
+		regexp.MustCompile("  FULLTEXT KEY "):                           []byte("  INDEX "),
+		regexp.MustCompile("ON UPDATE CURRENT_TIMESTAMP"):               []byte(""),
+		regexp.MustCompile(" ON DELETE CASCADE"):                        []byte(""),
+		regexp.MustCompile("DEFAULT b'0',$"):                            []byte("DEFAULT 0,"),
+		regexp.MustCompile("DEFAULT b'1',$"):                            []byte("DEFAULT 1,"),
+		regexp.MustCompile(`int DEFAULT '(.*)',$`):                      []byte("INT DEFAULT $1,"),
+		regexp.MustCompile(`int NOT NULL DEFAULT '(.*)',$`):             []byte("INT NOT NULL DEFAULT $1,"),
+		regexp.MustCompile(`\(decimal(.*)\) NOT NULL DEFAULT '(.*)',$`): []byte("$1 NOT NULL DEFAULT $2,"),
+		regexp.MustCompile(`\(decimal(.*)\) DEFAULT '(.*)',$`):          []byte("$1 DEFAULT $2,"),
+		regexp.MustCompile("  UNIQUE KEY "):                             []byte("  UNIQUE INDEX "),
+	}
+
+	blankLines     = regexp.MustCompile(`\s\s\n`)
+	constraints    = regexp.MustCompile("(?smU)CONSTRAINT.*REFERENCES.*$")
+	tables         = regexp.MustCompile("(?smU)^(CREATE|INSERT).*;$")
+	trailingCommas = regexp.MustCompile(`,\n\);`)
+)
 
 type roacher struct {
 	sourceData []byte
@@ -42,6 +52,26 @@ func (r *roacher) roach() (output []byte, err error) {
 	for pattern, replacement := range simpleReplacements {
 		output = pattern.ReplaceAll(output, replacement)
 	}
+
+	// Split the source data down by table.
+	tables := tables.FindAll(output, -1)
+
+	var tableConstraints [][]byte
+	for i := range tables {
+		t := &tables[i]
+
+		// TODO: We need to allocate constraints against the table name here.
+
+		// Extract the constraints from each table definition.
+		tableConstraints = append(tableConstraints, constraints.FindAll(*t, -1)...)
+		*t = constraints.ReplaceAll(*t, []byte(""))
+
+		// Tidy.
+		*t = blankLines.ReplaceAll(*t, []byte(""))
+		*t = trailingCommas.ReplaceAll(*t, []byte("\n);"))
+	}
+
+	output = bytes.Join(tables, []byte("\n"))
 
 	return
 }
